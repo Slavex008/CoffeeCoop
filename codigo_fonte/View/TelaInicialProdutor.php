1138fb6435013b3d14af290f434df780e32c48b7
Slavex008 <arthucruz@hotmail.com>
2018-01-26 02:33:59 -0200

    melhora da interface

--- codigo_fonte/View/TelaInicialProdutor.php
+++ codigo_fonte/View/TelaInicialProdutor.php
@@ -10,7 +10,7 @@
     $link = $conexao->getLink();
     
     session_start();
-    $idProdutor = $_SESSION["idProdutor"];
+    $idProdutor = $_SESSION["user"];
     
     $sacaDeCafeDao = new SacaDeCafeDAO();
     $retorno = $sacaDeCafeDao->buscarSacas($idProdutor, $link);
@@ -22,44 +22,39 @@
 <html>
     <head>
         <title>CoffeeCoop</title>
+        <link rel = 'stylesheet' href = 'CSS/style.css'>
     </head>
 
     <body>
         
-    <div>
+    <div class='telainicial' align='center'>
         <form>
             <fieldset>
-                <input name="tipo" placeholder = "Digite o tipo de café" type = "text" maxlength = "40" required autofocus>
-                <button name = "submit" type = "submit">Consultar</button>
+                <input class='inputbusca' name="tipo" placeholder = "Digite o tipo de café" type = "text" maxlength = "40" required autofocus>
+                <button name = "submit" type = "submit" class='btnbusca'>Consultar</button>
             </fieldset>
         
         </form>
     
-    </div>
-    
-    <div>
-    <table border = '1'>
-        <tr>
-            <th>Tipo</th>
-            <th>Data de Armazenamento</th>
-            <th>Quantidade</th>
-        </tr>
-        <?php
-            foreach($rs as $linha){
-                $indice = $linha[0];
-                echo "<tr>";
-                echo "<td>".$linha[1]."</td>";
-                echo "<td>".$linha[2]."</td>";
-                echo "<td>".$linha[3]."</td>";
-                echo "<td><a href='EditarCafe.php?id=".$indice."'><button name = 'e".$indice."' type = 'submit'>E</button></a></td>";
-                
-                echo "</tr>";
-            }   
-        ?>
-    </table>
-    </div>
-    
-    <div>
+        <table border = '2' class='tabela' align='center'>
+            <tr>
+                <th>Tipo</th>
+                <th>Data de Armazenamento</th>
+                <th>Quantidade</th>
+            </tr>
+            <?php
+                foreach($rs as $linha){
+                    $indice = $linha[0];
+                    echo "<tr>";
+                    echo "<td>".$linha[1]."</td>";
+                    echo "<td>".$linha[2]."</td>";
+                    echo "<td>".$linha[3]."</td>";
+                    echo "<td><a href='EditarCafe.php?id=".$indice."'><button name = 'e".$indice."' type = 'submit'>E</button></a></td>";
+                    
+                    echo "</tr>";
+                }   
+            ?>
+        </table>
         <a href="EstocarCafe.html"><button name = "submit" type = "submit">Inserir</button></a>
         <button name = "submit" type = "submit">Retirar</button>
         <button name = "submit" type = "submit">Vender</button>
